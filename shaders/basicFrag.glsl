2215ecee00dd8eff13e5b52790cf7dca899d927a
maxD <maxd@nnvtn.ca>
2016-01-27 13:16:21 -0500

    basicFrag

--- shaders/basicFrag.glsl
+++ shaders/basicFrag.glsl
@@ -11,9 +11,19 @@ uniform vec2 texOffset;
 
 varying vec4 vertColor;
 varying vec4 vertTexCoord;
+// use these, but not here...
+uniform float u1;
+uniform float u2;
+uniform float u3;
+uniform float u4;
+
+vec2 rot(vec2 p, float a) {
+	return vec2(
+		p.x * cos(a) - p.y * sin(a),
+		p.x * sin(a) + p.y * cos(a));
+}
 
 void main(void) {
   vec2 pos = vertTexCoord.xy;
-  vec4 col = texture2D(texture, pos);
-	gl_FragColor = col;
+	gl_FragColor = texture2D(texture, pos);
 }
